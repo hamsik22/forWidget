bd73a07a25c6aa19dad6cdfbe7b45ac5ada3c185
HwangSukHyeon <payback9488@gmail.com>
2025-01-06 14:49:51 +0900

    proj: 프로젝트 생성

---
+++ ForWidget/ForWidgetTests/ForWidgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  ForWidgetTests.swift
+//  ForWidgetTests
+//
+//  Created by 황석현 on 1/6/25.
+//
+
+import Testing
+@testable import ForWidget
+
+struct ForWidgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
